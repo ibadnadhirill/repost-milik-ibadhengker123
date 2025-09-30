7c46514a1c7d9199535e7b5cecf328e7590530b8
ibadnadhirill <ibadnadhirill@gmail.com>
2025-09-30 14:06:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sapa = document.getElemenById('sapaButton')
+const sapa = document.getElemenById('sapa')
 sapa.addEventListener('click,function(){
                       alert('halo! thank wes mrene')
 });
